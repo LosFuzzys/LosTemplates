f41628042bb71ad9f719f0c98ed1038365ec15c6
Ernesto Martinez Garcia <git@ecomaikgolf.com>
2025-06-19 19:08:59 +0200

    sagemath fix loading .sage

--- sagemath-nojail-ubuntu22.04/challenge/entrypoint.sh
+++ sagemath-nojail-ubuntu22.04/challenge/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-. /home/sage/.venv/bin/activate 2>&1 >/dev/null
-export PATH="/home/sage/.venv/bin:$PATH"
+
+# This should be the correct thing to do but it doesn't work
+# . /home/sage/.venv/bin/activate 2>&1 >/dev/null
+# export PATH="/home/sage/.venv/bin:$PATH"
+
 /usr/bin/stdbuf -i0 -o0 -e0 sage /app/challenge
